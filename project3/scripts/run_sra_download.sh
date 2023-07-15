5a1ba342e777a348bd3023642f3fa5a4fb52c166
sun-xinh <sun.xinh@northeastern.edu>
2023-07-15 08:48:03 -0400

    Over_MaxArraySize

---
+++ project3/scripts/run_sra_download.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+
+################################################################################
+# This Bash script is help to submit to Slurm in the number of sra exceeds 
+# the maximum number of array size
+# Download fastq files from SRA for PRJNA436229. !YOUR SRA NUMBER
+# Author: Xinhao Sun
+# 2023-06-14
+################################################################################
+
+##### VARIABLES #####
+if [ $# -lt 1 ]
+then
+    echo "need a samples file (sra number)"
+    exit 1 
+fi
+# Destination directory where files will be downloaded.
+DEST_DIR=/scratch/$USER/singlecellrna/project3/data
+
+# Change this to the path where you stored your samples list
+ACC_FILE=/scratch/$USER/singlecellrna/project3/$1
+
+######
+
+if [[ ! -e $ACC_FILE ]]; then
+    echo "File $ACC_FILE does not exist."
+    exit 2
+fi 
+
+# Create the destination directory.
+mkdir -p ${DEST_DIR}
+
+
+# Calculate total number of samples and set the max array size
+TOTAL_SAMPLES=$(wc -l < $ACC_FILE)
+
+echo "Number of sra is $TOTAL_SAMPLES" 
+
+MAX_ARRAY_SIZE=500
+OFFSET=0
+
+while [[ $TOTAL_SAMPLES -gt 0 ]]
+do
+    LIMIT=$(( TOTAL_SAMPLES > MAX_ARRAY_SIZE ? MAX_ARRAY_SIZE - 1 : TOTAL_SAMPLES - 1 ))
+    sbatch --array=0-$LIMIT 1_sra_download.sh $1 $OFFSET
+    OFFSET=$((OFFSET + (LIMIT + 1) ))
+    TOTAL_SAMPLES=$(( TOTAL_SAMPLES - (LIMIT + 1) ))
+done
--- project3/scripts/sra_download.sh
+++ project3/scripts/sra_download.sh
@@ -3,38 +3,27 @@
 #SBATCH --partition=short
 #SBATCH --export=ALL
 #SBATCH --nodes 1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
+#SBATCH --ntasks 1
 #SBATCH --mem 32G
 #SBATCH --time 24:00:00
-#SBATCH --out=/scratch/sun.xinh/singlecellrna/project3/scripts/%x_%j.log
-#SBATCH --err=/scratch/sun.xinh/singlecellrna/project3/scripts/%x_%j.err
+#SBATCH --out=/scratch/sun.xinh/singlecellrna/project2/logs/%x_%j.log
+#SBATCH --err=/scratch/sun.xinh/singlecellrna/project2/logs/%x_%j.err
 #SBATCH --mail-type=ALL   # get email updates about the job
 #SBATCH --mail-user=sun.xinh@northeastern.edu
-
-##### the number of sra exceeds the maximum number of array size
-##SBATCH --array=1-768 # Change to represent the total number of samples in your file
+#SBATCH --array=1-768 # Change to represent the total number of samples in your file
 ################################################################################
 # Download fastq files from SRA for PRJNA436229.
 # 2023-06-14
 ################################################################################
 
-if [ $# -lt 2 ]
-then
-    echo "need a samples file (sra number) and a start number"
-    exit 1 
-fi
-
 ##### VARIABLES #####
 
 # Destination directory where files will be downloaded.
-DEST_DIR=/scratch/$USER/singlecellrna/project3/data
+DEST_DIR=/scratch/$USER/singlecellrna/project2/data
 
 # Change this to the path where you stored your samples list
-ACC_FILE=/scratch/$USER/singlecellrna/project3/$1
-OFFSET=$2
-NUM_ROW=$((OFFSET + SLURM_ARRAY_TASK_ID))
-ACC_NUM=$(sed "${NUM_ROW+1}q;d" ${ACC_FILE})
+ACC_FILE=/scratch/$USER/singlecellrna/project2/$1
+ACC_NUM=$(sed "${SLURM_ARRAY_TASK_ID}q;d" ${ACC_FILE})
 
 ######
 
@@ -50,13 +39,13 @@ export LC_ALL=en_US.UTF-8
 
 
 # Download files.
-echo "fastq-dump \
+fastq-dump \
     --dumpbase \
     --readids \
     --split-files \
     --gzip \
     --outdir ${DEST_DIR} \
-    ${ACC_NUM}"
+    ${ACC_NUM}
 
 # If 10X, Rename files for further use
 #mv ${DEST_DIR}/${ACC_NUM}_1.fastq.gz ${DEST_DIR}/${ACC_NUM}_S1_L001_I1_001.fastq.gz
